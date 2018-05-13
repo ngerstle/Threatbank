54715ff8a4ba6537ff517493beb74691e18c1a71
ngerstle@d598s-l-d <ngerstle@gmail.com>
2018-05-13 02:16:27 +0200

    refactor

--- App.elm
+++ App.elm
@@ -1,12 +1,14 @@
 module Main exposing (..)
 
+-- exposing (..)
+
 import Base64
 import Html
 import Json.Encode
 import List.Extra
 import Navigation
 import Rest exposing (..)
-import Threat exposing (..)
+import Threat
 import Types exposing (..)
 import View exposing (..)
 
@@ -64,7 +66,7 @@ validateModel m =
 -- could add which threats are invalid and why
 
 
-updateSubThreat : Model -> Result ( Threat, String ) Threat -> Model
+updateSubThreat : Model -> Result ( Threat.Threat, String ) Threat.Threat -> Model
 updateSubThreat model threatresult =
     {- Replaces an existing Threat with an updated Threat -}
     let
@@ -85,10 +87,10 @@ updateSubThreat model threatresult =
     { model | threats = newthreats, status = status }
 
 
-updateThreat : ThreatFieldId -> String -> Model -> Model
+updateThreat : Threat.ThreatFieldId -> String -> Model -> Model
 updateThreat threatid text model =
     let
-        getSubThreat : Model -> ThreatFieldId -> Maybe Threat
+        getSubThreat : Model -> Threat.ThreatFieldId -> Maybe Threat.Threat
         getSubThreat model threatfieldid =
             {- Gets a threat based on an EditField event -}
             let
@@ -108,7 +110,7 @@ updateThreat threatid text model =
             { model | status = "no threat by that id" }
 
 
-setid : List Threat -> List Threat
+setid : List Threat.Threat -> List Threat.Threat
 setid threatList =
     let
         maxid =
@@ -137,12 +139,11 @@ update msg model =
     case msg of
         Generate CSV ->
             let
-                -- cmd = Cmd.none
                 activeThreats =
                     List.filter .selected model.threats
 
                 rawpayload =
-                    exportThreatsCSV activeThreats
+                    Threat.exportThreatsCSV activeThreats
 
                 payload64 =
                     Base64.encode rawpayload
@@ -153,9 +154,7 @@ update msg model =
             ( { model | status = "generate CSV file" }, cmd )
 
         Generate JSON ->
-            --TODO redirect to dataurl with json
             let
-                -- cmd = Cmd.none
                 rawpayload =
                     Json.Encode.encode 2 (modelEncoder model)
 
